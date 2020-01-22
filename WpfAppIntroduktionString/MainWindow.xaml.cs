baf81eae33f180c40c2d4f7e0ab320af9fd523d7
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2020-01-22 11:08:29 +0100

    textBox1 in MainWindow gets text from the GetTextForTextBox(TextBox textBox) method in the ClassBIZ class

--- WpfAppIntroduktionString/MainWindow.xaml.cs
+++ WpfAppIntroduktionString/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAppIntroduktionString.BIZ;
 
 namespace WpfAppIntroduktionString
 {
@@ -23,6 +24,8 @@ namespace WpfAppIntroduktionString
         public MainWindow()
         {
             InitializeComponent();
+            ClassBIZ classBIZ = new ClassBIZ();
+            classBIZ.GetTextForTextBox(textBox1);
         }
     }
 }
